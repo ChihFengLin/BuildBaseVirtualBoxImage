76146e98eda7bb27cc126700e088c58b828e5c3c
Chih-Feng Lin <chihfenl@alumni.cmu.edu>
2021-06-01 11:33:18 -0400

    type with virtual env

--- boot/bootstrap-vip_mgmnt.sh
+++ boot/bootstrap-vip_mgmnt.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 -m ven ~/ansible; source ~/ansible/bin/activate
+python3 -m venv ~/ansible; source ~/ansible/bin/activate
 
 # Install Ansible
 pip install setuptools --upgrade
 pip install pip --upgrade
-pip install pycparser functools32 pytz ansible==4.0.0
+pip install ansible==4.0.0
 
 # Install Salt Master and Configure
 sudo yum -y install https://repo.saltstack.com/yum/redhat/salt-repo-latest-2.el7.noarch.rpm
